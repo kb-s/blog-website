d94889cb21957199806ae99b59a13dd4128dcf93
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2023-03-31 12:17:41 +0530

    create write component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar"
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 function App() {
   return (
     <>
     <Topbar/>
-    <Single/>
+    <Write/>
     </>
   );
 }
