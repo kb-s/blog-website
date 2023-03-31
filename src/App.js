f54ae758dc6e2fae796617b7815c8d7ff3a65eb3
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2023-03-31 13:12:54 +0530

    login component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Topbar from "./components/topbar/Topbar"
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <>
     <Topbar/>
-    <Setting/>
+    <Login/>
     </>
   );
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import "./login.css"
+
+function Login() {
+  return (
+    <div className="login">
+        <span className="loginTitle">Login</span>
+        <form className="loginForm">
+            <label>Email</label>
+            <input type="text" className="loginInput" placeholder="Enter Your email..."/>
+            <label>Password</label>
+            <input type="password" className="loginInput" placeholder="Enter Your password..."/>
+            <button className="loginButton">Login</button>
+        </form>
+        <button className="registerButton">Register</button>
+    </div>
+  )
+}
+
+export default Login
