bb6bbe15c16a224d448027f10b49b04cf5a21ffb
kanhaiyabhatt1 <kbhattcoder@gmail.com>
2023-03-31 12:52:43 +0530

    settings component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar"
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Settings";
 function App() {
   return (
     <>
     <Topbar/>
-    <Write/>
+    <Setting/>
     </>
   );
 }
